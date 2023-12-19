bdbf4ca619701a10237626faa6dbf59e1dba5563
aliya <alexdavydov92@gmail.com>
2023-12-19 23:06:27 +0300

    added demoqa test

--- conftest.py
+++ conftest.py
@@ -1,13 +1,14 @@
-from selene import Browser, Config
+from selene import browser
 import pytest
-from selene.support import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver import Remote
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_config(request):
-
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1440
+    browser.config.window_height = 900
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -17,15 +18,12 @@ def browser_config(request):
     options.capabilities.update(selenoid_capabilities)
 
     # Использовать класс Remote из модуля selenium.webdriver, а не selene.support.webdriver
-    driver = Remote(
+    driver = webdriver.Remote(
         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options,
     )
 
-    browser = Browser(Config(driver))
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1440
-    browser.config.window_height = 900
+    browser.config.driver = driver
 
     yield
 
